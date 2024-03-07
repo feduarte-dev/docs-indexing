b0a78391c7959227d150d970ec1cd60ba6359893
Felipe Duarte <feduarte.dev@gmail.com>
2024-03-07 18:52:09 -0300

    requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+
+    if path_file[-3:] != "txt":
+        print("Formato inválido", file=sys.stderr)
+    try:
+        with open(path_file, "r") as file:
+            lines = [line.strip() for line in file]
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+    return lines
+
+
+print(txt_importer("statics/nomde_pedro.txt"))
