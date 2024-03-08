46e0c7bf9e7760660a4bbfdf0ea557f332915596
Felipe Duarte <feduarte.dev@gmail.com>
2024-03-08 00:14:10 -0300

    requisito 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -16,5 +16,19 @@ def exists_word(word, instance):
     ]
 
 
-def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+def search_by_word(word, instance):  # nao me parece muito clean
+    return [
+        {
+            "palavra": word,
+            "arquivo": item["nome_do_arquivo"],
+            "ocorrencias": [
+                {"linha": i + 1, "conteudo": line}
+                for i, line in enumerate(item["linhas_do_arquivo"])
+                if word.lower() in line.lower()
+            ],
+        }
+        for item in instance._data
+        if any(
+            word.lower() in line.lower() for line in item["linhas_do_arquivo"]
+        )
+    ]
